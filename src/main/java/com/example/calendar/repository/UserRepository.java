c0590a83e8f3d53dd94309c455321fe72cfd3e7a
Stefan Butacu <stefbutacu@gmail.com>
2022-12-18 15:21:17 +0200

    Fix bug on login with invalid credentials

--- src/main/java/com/example/calendar/repository/UserRepository.java
+++ src/main/java/com/example/calendar/repository/UserRepository.java
@@ -2,17 +2,16 @@ package com.example.calendar.repository;
 
 
 import com.example.calendar.model.User;
-import com.example.calendar.model.Event;
-import com.example.calendar.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
     boolean existsByUsername(String username);
-    User getById(String username);
+    Optional<User> getUserByUsername(String username);
 }
